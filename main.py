53ae49ee7b943442372918b6543ed6a3b39b02a2
taki <takoreset@gmail.com>
2019-10-01 00:55:23 +0900

    長い音声データにも対応

--- main.py
+++ main.py
@@ -3,44 +3,58 @@ from google.cloud.speech import types
 from google.cloud.speech import enums
 from google.cloud import speech
 import io
-import os
 
-DATA_DIR_PATH = os.path.join(
-    os.path.dirname(__file__),
-    'data'
-)
-
-MP3_FILE_PATH = DATA_DIR_PATH + '/speech_origin.mp3'
-FLAC_FILE_PATH = DATA_DIR_PATH + '/speech.flac'
+MP3_FILE_PATH = 'data/origin3.mp3'
 
 client = speech.SpeechClient()
 
 
-def speech_to_text(speech_flac_file: str) -> str:
-    """音声ファイルの文字おこしをする
+class SpeechToText:
+    def create_config(self):
+        return types.RecognitionConfig(
+            encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+            sample_rate_hertz=16000,
+            language_code='ja-JP')
 
-    Args:
-        speech_flac_file (str): flac形式の音声ファイルパス
+    def transcribe_file(self, speech_flac_file: str) -> None:
+        """音声ファイルの文字おこしをする
 
-    Returns:
-        str: 文字お越し結果
-    """
+        Args:
+            speech_flac_file (str): flac形式の音声ファイルパス
+        """
 
-    with io.open(speech_flac_file, 'rb') as audio_file:
-        content = audio_file.read()
+        with io.open(speech_flac_file, 'rb') as audio_file:
+            content = audio_file.read()
 
-    audio = types.RecognitionAudio(content=content)
-    config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
-        sample_rate_hertz=44100,
-        language_code='ja-JP')
+        audio = types.RecognitionAudio(content=content)
+        config = self.create_config()
 
-    response = client.recognize(config, audio)
-    # Each result is for a consecutive portion of the audio. Iterate through
-    # them to get the transcripts for the entire audio file.
-    for result in response.results:
-        # The first alternative is the most likely one for this portion.
-        print(result.alternatives[0].transcript)
+        self.parse_response(
+            client.recognize(config, audio)
+        )
+
+    def long_transcribe_gcs(self, gcs_uri: str) -> None:
+        """1分以上の長い音声ファイルの文字おこしをする
+
+        Args:
+            gcs_uri (str): gcsの音声ファイルのパス
+        """
+
+        audio = types.RecognitionAudio(uri=gcs_uri)
+        config = self.create_config()
+
+        operation = client.long_running_recognize(config, audio)
+
+        print('Waiting for operation to complete...')
+
+        self.parse_response(
+            operation.result(timeout=90)
+        )
+
+    def parse_response(self, response):
+        for result in response.results:
+            print(u'Transcript: {}'.format(result.alternatives[0].transcript))
+            print('Confidence: {}'.format(result.alternatives[0].confidence))
 
 
 def convert_mp3_to_flac(mp3_file_path: str) -> str:
@@ -54,12 +68,14 @@ def convert_mp3_to_flac(mp3_file_path: str) -> str:
     """
     sound = AudioSegment.from_mp3(mp3_file_path)
 
+    flac_file_path = mp3_file_path.replace('.mp3', '.flac')
+
     sound.export(
-        FLAC_FILE_PATH,
+        flac_file_path,
         format="flac",
         parameters=["-ac", "1"])
 
-    return FLAC_FILE_PATH
+    return flac_file_path
 
 
 if __name__ == "__main__":
@@ -67,4 +83,6 @@ if __name__ == "__main__":
         MP3_FILE_PATH
     )
 
-    speech_to_text(speech_flac_file)
+    speech_to_text = SpeechToText()
+    speech_to_text.transcribe_file(speech_flac_file)
+    # speech_to_text.long_transcribe_gcs("gs://audio_data_for_speech_to_text/speech.flac")
